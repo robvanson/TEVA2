857e509c26b93546d87512c2f47b471656e8729c
Rob van Son <R.J.J.H.vanSon@gmail.com>
2016-04-09 16:55:33 +0200

    Converted blob to array and draw waveform

--- audioProcessing.js
+++ audioProcessing.js
@@ -16,7 +16,6 @@
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 var setDrawingParam = function (canvasId) {
-console.log(canvasId);
 	var drawingArea = document.getElementById(canvasId);
 	var drawingCtx = drawingArea.getContext("2d");
 	return drawingCtx;
@@ -31,7 +30,7 @@ var initializeDrawingParam = function (canvasId) {
 
 var resetDrawingParam = function (drawingCtx) {
 	drawingCtx.clearRect(0, 0, drawingCtx.canvas.width, drawingCtx.canvas.height);
-	drawingCtx.lineWidth = 1;
+	drawingCtx.lineWidth = 0.5;
 	drawingCtx.strokeStyle = "black";
 	drawingCtx.lineCap = "round";
 	drawingCtx.lineJoin = "round";
@@ -45,6 +44,89 @@ var testDrawing = function (canvasId, color, order) {
 	drawingCtx.lineTo(750 - order,750);
 	drawingCtx.stroke();
 };
-	
-	
 
+function draw_waveform (canvasId, color, typedArray, tmin, tmax) {
+	var drawingCtx = setDrawingParam(canvasId);
+	var plotWidth = 0.95 * drawingCtx.canvas.width;
+	var horMargin = 0.05 * drawingCtx.canvas.width;
+	var plotHeight = 0.9 * drawingCtx.canvas.height
+	var verMargin = 0.05 * drawingCtx.canvas.height
+	var tickLength = 10;
+	
+	var verMax = 1;
+	var verMin = -1;
+	var deltaVer = 0.1 * plotHeight;
+	var horMin = tmin;
+	var horMan = tmax;
+	var deltaHor = plotWidth / 20;
+	
+	// Set parameters
+	resetDrawingParam(drawingCtx);
+	drawingCtx.beginPath();
+	drawingCtx.strokeStyle = color;
+	
+	var numSamples = typedArray.length;
+	
+	// Draw axes
+	drawingCtx.beginPath();
+	drawingCtx.lineWidth = 0.5;
+	drawingCtx.moveTo(horMargin, 0);
+	drawingCtx.lineTo(horMargin, plotHeight);
+	drawingCtx.lineTo(horMargin + plotWidth, plotHeight);
+	drawingCtx.stroke();
+	// Draw ticks
+	// Vertical
+	drawingCtx.beginPath();
+	drawingCtx.lineWidth = 0.1;
+	for (var v = 0; v <= plotHeight; v += deltaVer) {
+		drawingCtx.moveTo(horMargin - tickLength, v);
+		drawingCtx.lineTo(horMargin, v);
+		drawingCtx.stroke();
+	};
+	// Horizontal
+	drawingCtx.beginPath();
+	drawingCtx.lineWidth = 0.1;
+	for (var h = 0; h <= plotWidth; h += deltaHor) {
+		drawingCtx.moveTo(horMargin + h, plotHeight);
+		drawingCtx.lineTo(horMargin + h, plotHeight + 2*tickLength);
+		drawingCtx.stroke();
+	};
+
+	// Reset drawing
+	drawingCtx.beginPath();
+	drawingCtx.lineWidth = 0.5;
+	
+	// Scale to plot area
+	var tScale = plotWidth / numSamples;
+	var vScale = plotHeight / 2;
+	
+	drawingCtx.moveTo(horMargin, verMargin + plotHeight / 2 - typedArray[0] * vScale);
+	for(var i = 1; i < numSamples; i+=1) {
+		var currentTime = tmin + i * tScale;
+		var currentValue = typedArray[i];
+		drawingCtx.lineTo(horMargin + i * tScale , plotHeight / 2 - currentValue * vScale);
+		prevTime = currentTime;
+		prevValue = currentValue;
+	};
+	drawingCtx.stroke();
+};
+// Decode the audio blob
+var audioProcessing_decodedArray;
+function processAudio (blob) {
+	var audioContext = new AudioContext();
+	var reader = new FileReader();
+	reader.onload = function(){
+		var arrayBuffer = reader.result;
+		audioContext.decodeAudioData(arrayBuffer, decodedDone);
+	};
+	reader.readAsArrayBuffer(blob);
+};
+
+function decodedDone(decoded) {
+	var typedArray = new Float32Array(decoded.length);
+	typedArray=decoded.getChannelData(0);
+	recordedArray = typedArray;
+	
+	// Process and draw audio
+	draw_waveform ("DrawingArea", "black", recordedArray, 0, 1)
+};

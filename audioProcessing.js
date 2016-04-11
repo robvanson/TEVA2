7c312c2440cb96fd0b5ded9e6a071ef3b4c4fa8f
Rob van Son <r.j.j.h.vanson@gmail.com>
2016-04-11 16:30:25 +0200

    Added Ltas

--- audioProcessing.js
+++ audioProcessing.js
@@ -103,6 +103,7 @@ function drawSignal (display) {
 		} else if (display == "Pitch") {
 		} else if (display == "Spectrogram") {
 		} else if (display == "Ltas") {
+			draw_ltas (canvasId, color, recordedArray, recordedSampleRate, recordedDuration);		
 		} else if (display == "Intensity") {
 			draw_intensity (canvasId, color, recordedArray, recordedSampleRate, recordedDuration);		
 		} else {
@@ -265,6 +266,101 @@ function draw_intensity (canvasId, color, typedArray, sampleRate, duration) {
 		};
 	};
 	drawingCtx.stroke();
+};
+
+function draw_ltas (canvasId, color, typedArray, sampleRate, duration) {
+	var drawingCtx = setDrawingParam(canvasId);
+	var plotWidth = 0.95 * drawingCtx.canvas.width;
+	var horMargin = 0.02 * drawingCtx.canvas.width;
+	var plotHeight = 0.9 * drawingCtx.canvas.height
+	var verMargin = 0.02 * drawingCtx.canvas.height
+	var tickLength = 10;
+	
+	var fMin = 0;
+	var fMax = 2000; // sampleRate / 2;
+	var maxPower = 100;
+	var verMax = maxPower;
+	var verMin = 0;
+	var deltaVer = 0.1 * plotHeight;
+	var horMin = fMin;
+	var horMan = fMax;
+	var deltaHor = plotWidth / 20;
+	
+	// Calculate FFT
+	// This is stil just the power in dB.
+	var inputLength = typedArray.length;
+	var FFT_N = Math.pow(2, Math.ceil(Math.log2(inputLength))) * 2;
+	var input = new Float32Array(FFT_N * 2);
+	var output = new Float32Array(FFT_N * 2);
+	for (var i = 0; i < FFT_N; ++i) {
+		input [2*i] = (i < inputLength) ? typedArray [i] : 0;
+		input [2*i + 1] = 0; 
+	};
+	var fft = new FFT.complex(FFT_N, false);
+	var ifft = new FFT.complex(FFT_N, true);
+		
+	fft.simple(output, input, 1)
+	// ifft.simple(input, output, 1)
+
+	// Calculate the power spectrum
+	var powerSpectrum = new Float32Array(FFT_N);
+	var powerScaling = Math.log10(FFT_N) * 20
+	for(var i = 0; i < FFT_N; ++ i) {
+		var powerValue = (output[2*i]*output[2*i] + output[2*i+1]*output[2*i+1]);
+		powerSpectrum[i] = maxPower - Math.log10(powerValue) * -10 - powerScaling;
+	};
+
+	// Set parameters
+	resetDrawingParam(drawingCtx);
+	drawingCtx.beginPath();
+	drawingCtx.strokeStyle = color;
+	
+	var numSamples = 2*fMax/sampleRate * powerSpectrum.length ;
+	
+	// Draw axes
+	drawingCtx.beginPath();
+	drawingCtx.lineWidth = 0.5;
+	drawingCtx.moveTo(horMargin, 0);
+	drawingCtx.lineTo(horMargin, plotHeight);
+	drawingCtx.lineTo(horMargin + plotWidth, plotHeight);
+	drawingCtx.stroke();
+	// Draw ticks
+	// Vertical
+	drawingCtx.beginPath();
+	drawingCtx.lineWidth = 0.1;
+	for (var v = 0; v <= plotHeight; v += deltaVer) {
+		drawingCtx.moveTo(horMargin - tickLength, v);
+		drawingCtx.lineTo(horMargin, v);
+		drawingCtx.stroke();
+	};
+	// Horizontal
+	drawingCtx.beginPath();
+	drawingCtx.lineWidth = 0.1;
+	for (var h = 0; h <= plotWidth; h += deltaHor) {
+		drawingCtx.moveTo(horMargin + h, plotHeight);
+		drawingCtx.lineTo(horMargin + h, plotHeight + 2*tickLength);
+		drawingCtx.stroke();
+	};
+
+	// Reset drawing
+	drawingCtx.beginPath();
+	drawingCtx.lineWidth = 0.5;
+	
+	// Scale to plot area
+	var hScale = plotWidth / numSamples;
+	var vScale = plotHeight / verMax;
+	var resetLine = 0;
+	
+	drawingCtx.moveTo(horMargin, plotHeight - powerSpectrum[0] * vScale);
+	for(var i = 1; i < numSamples; ++i) {
+		var currentTime = fMin + i * hScale;
+		var currentValue = powerSpectrum[i];
+		if (currentValue > 0) {
+			drawingCtx.lineTo(horMargin + i * hScale , plotHeight - currentValue * vScale);
+		};
+	};
+	drawingCtx.stroke();
+	
 };
 
 // Decode the audio blob
